bf509c30ffb5c1ee61a2800f0fb7f19994627a09
unholyzara <zaramella.mattia@gmail.com>
2026-05-03 17:19:04 +0200

    Fixed s3 bucket name

--- global/backend.tf
+++ global/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "portfolio-terraform-state-ACCOUNT_ID"
+    bucket  = "portfolio-terraform-state"
     key     = "global/terraform.tfstate"
     region  = "eu-south-1"
     encrypt = true
