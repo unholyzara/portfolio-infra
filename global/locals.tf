f2a2708540e99ec85f5e6a76beb193c95ae56dfa
unholyzara <zaramella.mattia@gmail.com>
2026-05-06 14:33:47 +0200

    Implemented different ssm parameters for each environment

--- global/locals.tf
+++ global/locals.tf
@@ -1,8 +1,19 @@
 locals {
-  services     = yamldecode(file("${path.module}/../services.yml"))["services"]
+  services_file = yamldecode(file("${path.module}/../services.yml"))
+  services      = services_file["services"]
+  environments  = services_file["environments"]
+
   services_map = { for s in local.services : s.name => s }
   ecr_services = { for s in local.services : s.name => s if s.ecr }
-  parameters   = yamldecode(file("${path.module}/config/parameters.yml"))["parameters"]
+
+  parameters = yamldecode(file("${path.module}/config/parameters.yml"))["parameters"]
+  ssm_parameters = {
+    for pair in setproduct(local.environments, local.parameters) :
+    "${pair[0].name}-${pair[1].name}" => {
+      environment = pair[0].name
+      parameter   = pair[1]
+    }
+  }
 
   github_org = "unholyzara"
   aws_region = "eu-south-1"
