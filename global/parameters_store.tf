aabb0dc289447159b2a16875abe91a309aaafa2d
unholyzara <zaramella.mattia@gmail.com>
2026-05-07 07:56:44 +0200

    Fixed ssm parameters name

--- global/parameters_store.tf
+++ global/parameters_store.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   for_each = local.ssm_parameters
 
-  name = "portfolio-${each.each.value.environment}-${each.each.value.parameter.name}"
+  name = "portfolio/${each.each.value.environment}/${each.each.value.parameter.name}"
   type = each.value.type
 }
