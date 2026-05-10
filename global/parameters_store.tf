49b7e50eba9f9248aec7d37161be8318a84abc91
unholyzara <zaramella.mattia@gmail.com>
2026-05-10 21:29:45 +0200

    Fixed ssm pt2

--- global/parameters_store.tf
+++ global/parameters_store.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "parameter" {
   for_each = local.ssm_parameters
 
   name = "/portfolio/${each.value.environment}/${each.value.parameter.name}"
-  type = each.value.type
+  # type = each.value.type
 
   tags = {
     Env = each.value.environment
